5b38c305d4801deaa5a571a535ef161e96386915
Matt Sullivan <msullivanxi@gmail.com>
2014-05-22 01:46:36 -0400

    Major Site Usability update
    added all the pages linked them all together made things all play
    nicely

---
+++ js/matt_sullivan.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+  var container = document.querySelector('#container');
+  var msnry = new Masonry( container, {
+      // options
+      itemSelector: '.item'
+  });
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+  $("#fade").delay(1000).animate({"opacity": "1"}, 700);
+
+});
